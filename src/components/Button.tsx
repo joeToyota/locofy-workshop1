c0538d2e2428e0024313adb04e35722e221c0591
ammez <ammez>
2025-03-26 08:54:32 +0000

    Pushed from Locofy Builder

---
+++ src/components/Button.tsx
@@ -0,0 +1,42 @@
+import { useMemo } from "react";
+import PropTypes from "prop-types";
+import styles from "./Button.module.css";
+
+const Button = ({
+  className = "",
+  type = "unselected",
+  buttonBorder,
+  icon,
+  xXXXX,
+}) => {
+  const buttonStyle = useMemo(() => {
+    return {
+      border: buttonBorder,
+    };
+  }, [buttonBorder]);
+
+  return (
+    <button
+      className={[styles.button, className].join(" ")}
+      data-type={type}
+      style={buttonStyle}
+    >
+      <img className={styles.icon} loading="lazy" alt="" src={icon} />
+      <div className={styles.xxxxx}>{xXXXX}</div>
+    </button>
+  );
+};
+
+Button.propTypes = {
+  className: PropTypes.string,
+  icon: PropTypes.string,
+  xXXXX: PropTypes.string,
+
+  /** Variant props */
+  type: PropTypes.number,
+
+  /** Style props */
+  buttonBorder: PropTypes.string,
+};
+
+export default Button;
